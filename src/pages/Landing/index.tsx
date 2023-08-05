c26f84d56f9451be6fd40f7a71f45a5ee581f22c
WarrenWu4 <warrenweiwu04@gmail.com>
2023-08-05 10:01:43 -0500

    added dark mode to landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,8 @@
-import { FaMoon, FaClone } from "react-icons/fa"
+import { FaMoon, FaClone, FaSun } from "react-icons/fa"
 import { BsGoogle, BsArrowRight } from "react-icons/bs"
 import { GoogleAuthProvider, getRedirectResult, signInWithRedirect, signInAnonymously } from "firebase/auth"
 import { auth } from "../../firebase";
+import { useEffect, useState } from "react";
 
 export default function Landing() {
 
@@ -20,6 +21,23 @@ export default function Landing() {
         await signInAnonymously(auth)
     }
 
+    const switchTheme = () => {
+        console.log(localStorage.theme)
+        localStorage.theme = (localStorage.theme === "dark") ? "light":"dark"
+        document.documentElement.className = localStorage.theme
+        setCurrTheme(localStorage.theme)
+    }
+
+    const [currTheme, setCurrTheme] = useState(localStorage.theme)
+
+    useEffect(() => {
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark')
+          } else {
+            document.documentElement.classList.remove('dark')
+        }
+    }, [])
+
     return (
         <div className="w-screen h-screen flex flex-col">
 
@@ -28,8 +46,13 @@ export default function Landing() {
                 <div className="font-bold text-2xl px-5 py-4">JUMBO</div>
 
                 <div className="flex px-5 py-4 [&>*]:ml-4">
-                    <FaMoon size={24}/>    
-                    <FaClone size={24}/>
+                    <button type="button" onClick={switchTheme}>
+                        {(currTheme === "dark") ? <FaSun size={24}/> : <FaMoon size={24}/>}
+                    </button>
+
+                    <button type="button">
+                        <FaClone size={24}/>
+                    </button>
                 </div>
 
             </div>
@@ -38,7 +61,7 @@ export default function Landing() {
 
                 <div className="font-bold text-6xl px-20">a better way to flashcard</div>
 
-                <button onClick={googleSignIn} className="flex items-center border-4 border-black border-solid px-4 py-3 rounded-md text-lg font-bold mt-12 transition duration-500 hover:bg-black hover:text-white"> <BsGoogle className="mr-[0.8rem]"/> sign in with google</button>
+                <button onClick={googleSignIn} className="flex items-center border-4 border-black border-solid dark:border-white px-4 py-3 rounded-md text-lg font-bold mt-12 transition duration-500 hover:bg-black hover:text-white dark:hover:bg-white  dark:hover:text-black"> <BsGoogle className="mr-[0.8rem]"/> sign in with google</button>
 
                 <button onClick={anonymousSignIn} className="mt-4 flex items-center group">try anonymously <BsArrowRight className="ml-2 group-hover:translate-x-2 transition duration-500"/></button>
 
