8997ec24de655389ff6088663725ee57fefc151d
WarrenWu4 <warrenweiwu04@gmail.com>
2023-08-05 10:23:50 -0500

    added btn and logic for/start clone icon

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,8 +1,10 @@
 import { FaMoon, FaClone, FaSun } from "react-icons/fa"
 import { BsGoogle, BsArrowRight } from "react-icons/bs"
+import { IoClose } from "react-icons/io5"
 import { GoogleAuthProvider, getRedirectResult, signInWithRedirect, signInAnonymously } from "firebase/auth"
 import { auth } from "../../firebase";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Landing() {
 
@@ -28,7 +30,22 @@ export default function Landing() {
         setCurrTheme(localStorage.theme)
     }
 
+    const handleDashboard = () => {
+        // get current user
+        const user = auth.currentUser
+        // if user is already logged in navigate to dashboard
+        if (user) {
+            navigate("/")
+        }   
+        // otherwise showcase an error modal
+        else {
+            setShowError(true)
+        }
+    }
+
     const [currTheme, setCurrTheme] = useState(localStorage.theme)
+    const [showError, setShowError] = useState(false)
+    const navigate = useNavigate()
 
     useEffect(() => {
         if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -39,7 +56,7 @@ export default function Landing() {
     }, [])
 
     return (
-        <div className="w-screen h-screen flex flex-col">
+        <div className="w-screen h-screen flex flex-col relative">
 
             <div className="w-full py-6 px-8 flex justify-between items-center">
 
@@ -50,7 +67,7 @@ export default function Landing() {
                         {(currTheme === "dark") ? <FaSun size={24}/> : <FaMoon size={24}/>}
                     </button>
 
-                    <button type="button">
+                    <button type="button" onClick={handleDashboard} >
                         <FaClone size={24}/>
                     </button>
                 </div>
@@ -65,6 +82,13 @@ export default function Landing() {
 
                 <button onClick={anonymousSignIn} className="mt-4 flex items-center group">try anonymously <BsArrowRight className="ml-2 group-hover:translate-x-2 transition duration-500"/></button>
 
+            </div>
+
+            <div className={`px-4 py-3 text-base font-medium rounded-md bg-red-100 dark:bg-red-400 border-4 border-solid fixed bottom-40 left-1/2 -translate-x-1/2 border-red-200 dark:border-red-400 ${(showError) ? "flex" : "hidden"}`}>
+                Please login to go to your dashboard!
+                <button type="button" onClick={() => {setShowError(false)}} className="ml-3" >
+                    <IoClose size={20} />
+                </button>
             </div>
 
         </div>
