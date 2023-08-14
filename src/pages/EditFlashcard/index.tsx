51fd78384ac2473450c3c911ca0ab3f6fa06fd53
WarrenWu4 <warrenweiwu04@gmail.com>
2023-08-14 08:08:00 -0500

    form change

--- src/pages/EditFlashcard/index.tsx
+++ src/pages/EditFlashcard/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { DocumentData } from "firebase/firestore";
@@ -14,6 +14,12 @@ export default function EditFlashcard() {
     const [data, setData] = useState<DocumentData>()
     const [isLoading, setIsLoading] = useState<boolean>(true)
 
+    const [title, setTitle] = useState<string>("")
+    const [desc, setDesc] = useState<string>("")
+
+    const titleRef = useRef<HTMLInputElement>(null)
+    const descRef = useRef<HTMLInputElement>(null)
+
     useEffect(() => {
 
         // setting theme
@@ -23,9 +29,11 @@ export default function EditFlashcard() {
         const getData = async() => {
             const response = await GetFlashcards(set_id!)
             setData(response.data?.data)
+            setTitle(response.data?.data.title)
+            setDesc(response.data?.data.desc)
             setIsLoading(false)
         }
-
+        
         getData()
 
     }, [])
@@ -34,20 +42,27 @@ export default function EditFlashcard() {
         await updateFlashcards(set_id!, data!)
     }
 
+    const changeTitle = (e:any) => {
+        setTitle(e.target.value)
+    }
+    const changeDesc = (e:any) => {
+        setDesc(e.target.value)
+    }
+
     return (
         <div className="w-screen h-screen overflow-hidden flex">
             <Sidebar/>
             <div className="w-full h-full items-start flex text-4xl font-bold py-12 px-4 flex-col">
 
                 <div className="flex w-full justify-between items-center">
-                    <div className="w-full font-bold text-2xl border-4 border-solid border-black rounded-md px-4 py-2 mr-4 dark:border-white" >{data?.title}</div>
+                    <input type="text" value={(data === undefined) ? "":title} className="w-full font-bold text-2xl border-4 border-solid border-black rounded-md px-4 py-2 mr-4 dark:border-white" ref={titleRef} onChange={changeTitle}/>
                     <div className="w-fit h-fit flex [&>*]:mx-2">
                         <button type="button" className="h-[56px] aspect-square font-bold text-2xl border-4 border-solid border-black dark:border-white rounded-md flex items-center justify-center" ><FaClone size={24}/></button>
                         <button type="button" className="h-[56px] aspect-square font-bold text-2xl border-4 border-solid border-black dark:border-white rounded-md flex items-center justify-center" ><FaRegClone size={24}/></button>
                     </div>
                 </div>
 
-                <div className="mt-4 text-xl font-semibold w-full p-2 border-4 border-solid border-black dark:border-white rounded-md">{data?.desc}</div>
+                <input type="text" className="mt-4 text-xl font-semibold w-full p-2 border-4 border-solid border-black dark:border-white rounded-md" onChange={changeDesc} ref={descRef} value={(data === undefined) ? "":desc}/>
 
                 <div className="text-3xl font-bold mt-8 flex w-full justify-between items-center">
                     Cards:
