c8d9ae09f2a9ea4a049e740efe42773223a95e12
WarrenWu4 <warrenweiwu04@gmail.com>
2023-08-24 15:24:58 -0500

    readded edit and study components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,20 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import GetTheme from "../../lib/GetTheme";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import { useLocation } from "react-router-dom";
+import EditFlashcard from "../EditFlashcard";
+import StudyFlashcard from "../StudyFlaschard";
 
 export default function Dashboard() {
+
+    const [type, set_id] = useLocation().pathname.split("/").slice(2, 4)
+
+    const [content, setContent] = useState<JSX.Element>(<></>)
+
 
     useEffect(() => {
 
         GetTheme()
 
+        if (type === undefined) {
+            setContent(
+                <div className="w-full h-screen flex justify-center items-center text-4xl font-bold py-12 px-4">
+                    Select/add a set to get started!
+                </div>
+            )
+        }
+        else if (type === "edit") {
+            setContent(<EditFlashcard/>)
+        }
+        else if (type === "view") {
+            setContent(<StudyFlashcard/>)
+        }
 
-    }, [])
+    }, [type, set_id])
 
     return (
         <div className="w-screen h-screen overflow-x-hidden flex">
             <Sidebar/>
-            Select/add a set to get started!
+            {content}
         </div>
     )
 }
