25420917e76934ae4806adf4a02ac38e5a730df0
WarrenWu4 <warrenweiwu04@gmail.com>
2024-03-12 22:54:46 -0500

    updated styling settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-      animation: {
-        'bounce-fade-up': 'bounce-fade-up 1s ease-in-out infinite',
-        'bounce-fade-down': 'bounce-fade-down 1s ease-in-out infinite',
-      },
-      keyframes: {
-        'bounce-fade-up': {
-          '0%': { opacity: '0', transform: 'translateY(0.4rem)' },
-          '50%': { opacity: '1', transform: 'translateY(0rem)' },
-          '100%': { opacity: '0', transform: 'translateY(-0.4rem)'}
-        },
-        'bounce-fade-down': {
-          '0%': { opacity: '0', transform: 'translateY(-0.4rem)' },
-          '50%': { opacity: '1', transform: 'translateY(0rem)' },
-          '100%': { opacity: '0', transform: 'translateY(0.4rem)'}
-        }
-      },
       colors: {
         "dark": "#151715",
         "light": "#FFFFFF",
