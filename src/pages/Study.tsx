357154feaa97b2d06b038b6ff8c511e6f048f1d7
WarrenWu4 <warrenweiwu04@gmail.com>
2024-03-22 16:10:33 -0500

    added progress bar to study view

--- src/pages/Study.tsx
+++ src/pages/Study.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { Set, blankSet } from "../types";
 import { useNavigate, useParams } from "react-router-dom";
+import { FaA, FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 
 export default function Study() {
 
@@ -65,26 +66,26 @@ export default function Study() {
     return (
         <div className="w-full h-full px-16 flex flex-col justify-center items-center gap-y-10">
 
-            <div className="w-full flex justify-center items-center gap-x-10">
+            <div className="w-full flex justify-center items-center gap-x-10 max-w-screen-lg">
 
-                <button type="button" onClick={prevCard}>
-                
+                <button type="button" onClick={prevCard} className="w-12 h-12 text-4xl">
+                    <FaArrowLeft/>
                 </button>
 
-                <button type="button" onClick={nextFace} className="border-8 rounded-md relative flex justify-center items-center font-bold text-2xl">
+                <button type="button" onClick={nextFace} className="w-full aspect-[5/3] border-8 rounded-md relative flex justify-center items-center font-bold text-2xl">
 
                     {setData.flashcards[currCard].faces[currFace]}
 
                 </button>
 
-                <button type="button" onClick={nextCard}>
-
+                <button type="button" onClick={nextCard} className="w-12 h-12 text-4xl">
+                    <FaArrowRight/>
                 </button>
 
             </div>
 
-            <div className="w-full rounded-md bg-gray-400 relative">
-                <div className={`absolute top-0 left-0 h-full w-[${currCard/setData.flashcards.length}%]`}></div>
+            <div className="w-full h-1 rounded-md bg-gray-400 relative">
+                <div className={`absolute top-0 left-0 h-full bg-green-400`} style={{width: currCard/setData.flashcards.length*100+"%"}}></div>
             </div>
 
         </div>
